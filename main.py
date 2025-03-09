3d58ffe40937b315d1670784316a8e0b43de6be1
hum-projects <humaira2011ahmed@gmail.com>
2025-03-09 13:23:49 +0000

    sdfsfdsf

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from data_downloader import PopulationApp  # Import PopulationApp class
+from data_visualizer import GDPApp  # Import GDPApp class
 
 class TheDataAnalyser:
     def __init__(self, root):  
@@ -22,12 +23,27 @@ class TheDataAnalyser:
             command=self.open_population_app  # Calls the function
         )
         self.population_button.pack(fill="x", expand=True)  
+        
+        # Button to open GDPApp
+        self.gdp_button = tk.Button(
+            self.main_frame, 
+            text="See GDP of Countries", 
+            bg="blue", 
+            fg="white", 
+            command=self.open_gdp_app  # Calls the function
+        )
+        self.gdp_button.pack(fill="x", expand=True)  
 
     def open_population_app(self):
         new_window = tk.Toplevel(self.root)  # Create a new window
         PopulationApp(new_window)  # Open PopulationApp in the new window
 
+    def open_gdp_app(self):
+        new_window = tk.Toplevel(self.root)  # Create a new window
+        GDPApp(new_window)  # Open GDPApp in the new window
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = TheDataAnalyser(root)  
     root.mainloop()
+
